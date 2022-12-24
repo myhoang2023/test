84cc5b974bbf56dc4c02fd17fbcfaecf21050151
myhoang2023 <121327350+myhoang2023@users.noreply.github.com>
2022-12-24 11:00:13 +0700

    Add files via upload

---
+++ storage/framework/views/0a2ec559e0254dd380df0af1def2689f36cac225.php
@@ -0,0 +1,67 @@
+
+<?php $__env->startSection('title_login','Admin-Login'); ?>
+<?php $__env->startSection('content_login'); ?>
+<div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <?php if($errors->any()): ?>
+                                            <div class="alert alert-danger">
+                                                <ul>
+                                                    <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                                        <li><?php echo e($error); ?></li>
+                                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                                </ul>
+                                            </div>
+                                        <?php endif; ?>
+                                        <?php if(Session::has('error_login')): ?>
+                                            <div class="alert alert-danger">
+                                                <?php echo e(Session::get('error_login')); ?>
+
+                                            </div>
+                                        <?php endif; ?>
+                                    </div>
+                                    <form class="user" method="POST" action="<?php echo e(route('admin.handle.login')); ?>">
+                                        <?php echo csrf_field(); ?>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user"
+                                                name="username">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="exampleInputPassword" placeholder="Password" name="password">
+                                        </div>
+                                        
+                                        
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">login</button>
+                                        
+                                        
+                                    </form>
+                                    
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.login_layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH G:\laravel\laravel-demo\resources\views/login/form_login.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/505202b2a34ef6bf08b864ed1599f52c5264f72a.php
@@ -0,0 +1,10 @@
+
+<?php $__env->startSection('title','Dashboard'); ?>
+<?php $__env->startSection('content_row'); ?>
+    <div class="row">
+        <div class="col">
+            <h5>this is dashboard</h5>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.admin_layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH G:\laravel\laravel-demo\resources\views/admin/dashboard/index.blade.php ENDPATH**/ ?>
